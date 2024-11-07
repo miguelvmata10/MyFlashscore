57f222bb80fcb8e8e453f2b0e871cbce62cfe1bd
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-07 18:22:14 +0000

    Route to test the API

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const apiFootballReq = require('./services/apiFootball');
 const cors = require("cors"); // Import CORS module to manage cross-origin requests
 const corsOption = {
     origin: ["http://localhost:5173"] // Specifies allowed origins, only "http://localhost:5173" can access the API
@@ -7,9 +8,18 @@ const corsOption = {
 
 app.use(cors(corsOption)) // Apply CORS settings to enable controlled access to the API
 
-app.get("/api", (req, res) => {
-    res.json({teste: ["test1", "test2"]})
-});
+// Rota para testar o request
+app.get('/test-football-api', async (req, res) => {
+    const endpoint = 'leagues';  
+    const params = { id: '39', season: '2022' };  
+  
+    try {
+      const data = await apiFootballReq(endpoint, params);  
+      res.json(data);  
+    } catch (error) {
+      res.status(500).json({ message: 'Erro na requisição', error: error.message });
+    }
+  });
 
 app.listen(8080, () => {
     console.log("Listening in port 8080!")
--- server/services/apiFootball.js
+++ server/services/apiFootball.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 require('dotenv').config(); // ir buscar os dados da api-key e api-host
+console.log(process.env.API_FOOTBALL_KEY, process.env.API_FOOTBALL_HOST);
 
 // Função de requisição para a API externa
 const apiFootballReq = async (endpoint, params = {}) => {
     const config = {
         method: 'get',
-        url: `https://v3.football.api-sports.io/${endpoint}`,
+        url: `https://api-football-v1.p.rapidapi.com/v3/${endpoint}`,
         headers: {
           'x-rapidapi-key': process.env.API_FOOTBALL_KEY,
           'x-rapidapi-host': process.env.API_FOOTBALL_HOST
