a250c8e6c56b65aae17bc6bf99096111d5fa4496
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-16 23:43:35 +0000

    Test sidebar in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,15 @@ import { useState, useEffect } from 'react'
 import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import axios from "axios"
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
 
-  // teste para recebimento de dados da backend
-  const [teste, setTeste] = useState([]);
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api"); 
-    setTeste(response.data.teste);
-    console.log(response.data.teste);
-  }; 
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-
   return (
     <>
-      <div>
-        {teste.map((test, index) => (
-          <div key={index}>
-            <p>{test}</p>
-          </div>
-        ))}
-      </div>
-      <div className="container">
-            <h1 className="p-5 text-center">Olá, Bootstrap com React!</h1>
-            <button variant="primary">Clique aqui</button>
-        </div>
+      <Navbar/>
+      <Sidebar/>
     </>
   )
 }
