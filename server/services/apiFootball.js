95de9c3e72d35ad6bc8653239881c42a8a4443b0
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-17 16:30:14 +0000

    Redis added
    Redis added to the project to keep in cache new requests

--- server/services/apiFootball.js
+++ server/services/apiFootball.js
@@ -1,10 +1,31 @@
 const axios = require('axios');
+const redis = require('redis'); // redis para cache
 require('dotenv').config(); // ir buscar os dados da api-key e api-host
-console.log(process.env.API_FOOTBALL_KEY, process.env.API_FOOTBALL_HOST);
+
+const client = redis.createClient({
+  url: 'redis://localhost:6379'
+});
+
+client.connect()
+  .then(() => console.log('Redis conectado!'))
+  .catch(err => console.error('Erro ao conectar com o Redis: ', err));
 
 // Função de requisição para a API externa
 const apiFootballReq = async (endpoint, params = {}) => {
-    const config = {
+
+    // cria uma chave única que combina o endpoint e os parâmetros
+    const cackeKey = `apiFootball:${endpoint}:${JSON.stringify(params)}`;
+
+    try {
+      const cacheData = await client.get(cackeKey);
+
+      // se os dados estão em cache, retorna esses dados
+      if (cacheData) {
+        console.log('Resposta já estava em cache!');
+        return JSON.parse(cacheData);
+      }
+
+      const config = {
         method: 'get',
         url: `https://api-football-v1.p.rapidapi.com/v3/${endpoint}`,
         headers: {
@@ -13,14 +34,20 @@ const apiFootballReq = async (endpoint, params = {}) => {
         },
         params: params
       };
-      
-      try {
-        const response = await axios(config);
-        return response.data;
-      } catch (error) {
-        console.error('Erro no request:', error);
-        throw error;
-      }
+
+      const response = await axios(config);
+      const data = response.data;
+
+      // guarda a resposta na cache de forma persistente
+      await client.set(cackeKey, JSON.stringify(data));
+      console.log("Resposta guardada na cache.");
+
+      return data;
+    } 
+    catch (error) {
+      console.error('Erro no request:', error);
+      throw error;
+    }
 };
 
 module.exports = apiFootballReq;
