d477c313735dcbc8c70787c11fb5ac51568a6b05
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-17 20:06:51 +0000

    Addition of Controllers to retrieve all leagues and leagues per country

--- server/controllers/CompetitionsAndCountryController.js
+++ server/controllers/CompetitionsAndCountryController.js
@@ -11,6 +11,44 @@ const getCountries = async (req, res) => {
     }
 };
 
+// Endpoint que retorna todas as ligas disponíveis na API
+const getLeagues = async (req, res) => {
+    try {
+        const data = await apiFootballReq('leagues');
+        res.status(200).json(data);
+    } catch {
+        console.error("Erro em adquirir as ligas: ", error.messasge);
+        res.status(500).json({error: 'Erro no endpoint das ligas'})
+    }
+};
+
+// Endpoint que retorna as ligas de um país consoante o code (ex: PT) do país
+const getLeaguesPerCountry = async (req, res) => {
+    const countryCode = req.query.code;
+    console.log(countryCode);
+    if (!countryCode) {
+        return res.status(400).json({error: 'Não foi definido o code do respetivo país'});
+    }
+
+    try {
+        const data = await apiFootballReq('leagues', {code: countryCode});
+
+        if (!data.response || data.response.lenght === 0) {
+            return res.status(404).json({error: `Nenhuma liga com o respetivo código do país: ${countryCode}`});
+        }
+
+        res.status(200).json(data.response);
+    } catch (error) {
+        console.error('Erro ao obter as ligas: ', error);
+        res.status(500).json({error: 'Erro interno do servidor'});
+    }
+};
+
+
 module.exports = {
     getCountries,
+    getLeagues,
+    getLeaguesPerCountry,
 };
+
+
