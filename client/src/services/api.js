f46ab45729d8cde2515c8b80251b3b5d6f1331b3
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-18 21:42:43 +0000

    Addition of an abstraction for API calls

---
+++ client/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// todas as chamadas para a API têm o formato guardado em API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// será usado em todas as chamadas à API
+const api = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+export default api;
