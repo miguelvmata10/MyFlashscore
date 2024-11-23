115ebaa05f0bddb68f3889017fa1ae9a9d1fa2a7
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-23 19:11:50 +0000

    Logic to user interaction in league menu and the styles

--- client/src/components/League/League.jsx
+++ client/src/components/League/League.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import './League.css';
 import { useParams } from 'react-router-dom';
 import api from '../../services/api';
 import Container from 'react-bootstrap/esm/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { Image, Row, Col } from 'react-bootstrap';
+import { auto } from '@popperjs/core';
 
 const League = () => {
     // obter o id pela URL
     const { leagueID } = useParams();
     const [leagueData, setLeagueData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [selected, setSelected] = useState('classificacoes');
 
     const fetchLeagueData = async () => {
         try {
@@ -26,15 +31,50 @@ const League = () => {
         fetchLeagueData();
     }, [leagueID]);
 
+    const handleButtonState = (button) => {
+        setSelected(button);
+    };
+
+    const isActiveButton = (button) => {
+        return button === selected ? "active" : "";
+    }
 
     if (loading) {
         return <div>Carregando...</div>;
     }
 
+    // colocar um dropdown para escolher a epoca e depois consoante a epoca apresentar os dados
+    // Por predefinição aparece a epoca atual
+    // Se o user escolher outra epoca, o botão de 'Lista' desaparece
+    // Falta também fazer a verificação se é Taça ou liga
+
     return (
-        <Container fluid>
-            <Image src={leagueData[0].league.logo} width={100} height={100} alt="Logo da liga" />
-            <h1>{leagueData[0].league.name}</h1>
+        <Container className='container p-5 rounded-4'>
+            <Row className="align-items-center mb-3">
+                <Col xs="auto">
+                    <Image src={leagueData[0].league.logo} width={60} height={auto} alt="Logo da liga" />
+                </Col>
+                <Col>
+                    <h4 className="mb-0">{leagueData[0].league.name}</h4>
+                </Col>
+            </Row>
+            <Row>
+                <ButtonGroup size='md'>
+                    <Button className={isActiveButton('classificacoes')} onClick={() => handleButtonState('classificacoes')}>
+                        Classificações
+                    </Button>
+                    <Button className={isActiveButton('resultados')} onClick={() => handleButtonState('resultados')}>
+                        Resultados
+                    </Button>
+                    <Button className={isActiveButton('lista')} onClick={() => handleButtonState('lista')}>
+                        Lista
+                    </Button>
+                    <Button className={isActiveButton('arquivo')} onClick={() => handleButtonState('arquivo')}>
+                        Arquivo
+                    </Button>
+                </ButtonGroup> 
+                <hr />
+            </Row>
         </Container>
     );
 };
