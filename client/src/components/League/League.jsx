7d26d09e8a2c1ca5cb20f2d1643a296e3bffff5d
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-25 20:51:37 +0000

    Update in button interaction to use the personalized hook

--- client/src/components/League/League.jsx
+++ client/src/components/League/League.jsx
@@ -2,19 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './League.css';
 import { useParams } from 'react-router-dom';
 import api from '../../services/api';
+import useButtonGroup from '../../hooks/useButtonGroup';
 import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { Image, Row, Col } from 'react-bootstrap';
 import { auto } from '@popperjs/core';
 import Standings from './Standings';
+import Statistics from './Statistics';
+import Results from './Results';
+import Archive from './Archive';
+import ListOfGames from './ListOfGames';
 
 const League = () => {
     // obter o id pela URL
     const { leagueID } = useParams();
     const [leagueData, setLeagueData] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [selected, setSelected] = useState('classificacoes');
+    const { selected, handleButtonState, isActiveButton } = useButtonGroup('classificacoes');
 
     const fetchLeagueData = async () => {
         try {
@@ -32,19 +37,28 @@ const League = () => {
         fetchLeagueData();
     }, [leagueID]);
 
-    const handleButtonState = (button) => {
-        setSelected(button);
-    };
-
-    const isActiveButton = (button) => {
-        return button === selected ? "active" : "";
-    }
-
     if (loading) {
         return <div>Carregando...</div>;
     }
 
     const currentSeason = leagueData[0].seasons.find(season => season.current);
+
+    const renderComponent = () => {
+        switch (selected) {
+            case 'classificacoes':
+                return <Standings season={currentSeason.year} leagueID={leagueID} />;
+            case 'resultados':
+                return <Results  />;
+            case 'lista':
+                return <ListOfGames />;
+            case 'marcadores':
+                return <Statistics season={currentSeason.year} leagueID={leagueID} />;
+            case 'arquivo':
+                return <Archive />;
+            default:
+                return <div>Erro</div>
+        }
+    }
 
     return (
         <Container className='container p-5 rounded-4'>
@@ -78,7 +92,7 @@ const League = () => {
                 <hr />
             </Row>
             <Row>
-                <Standings season={currentSeason.year} leagueID={leagueID} />
+                {renderComponent()}
             </Row>
         </Container>
     );
