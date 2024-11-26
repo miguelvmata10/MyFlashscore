d9bc1a896e6cc0861231762a045dcf7cd870e43b
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-11-26 18:49:00 +0000

    Change of Archive to a dropdown where the user can select the season and see the data from that season

--- client/src/components/League/League.jsx
+++ client/src/components/League/League.jsx
@@ -6,30 +6,38 @@ import useButtonGroup from '../../hooks/useButtonGroup';
 import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { Image, Row, Col } from 'react-bootstrap';
-import { auto } from '@popperjs/core';
 import Standings from './Standings';
 import Statistics from './Statistics';
 import Results from './Results';
-import Archive from './Archive';
 import ListOfGames from './ListOfGames';
 
 const League = () => {
-    // obter o id pela URL
     const { leagueID } = useParams();
     const [leagueData, setLeagueData] = useState(null);
+    const [availableSeasons, setAvailableSeasons] = useState(null);
+    const [selectedSeason, setSelectedSeason] = useState(null);
     const [loading, setLoading] = useState(true);
     const { selected, handleButtonState, isActiveButton } = useButtonGroup('classificacoes');
 
     const fetchLeagueData = async () => {
         try {
-            const response = await api.get(`/competitions/leagues/${leagueID}`); 
+            const response = await api.get(`/competitions/leagues/${leagueID}`);
             setLeagueData(response.data.response);
-            console.log('Dados da liga:', leagueData);  
+
+            const seasons = response.data.response[0]?.seasons.map((season) => season.year);
+            setAvailableSeasons(seasons);
+
+            // Definir a temporada atual
+            const currentSeason = response.data.response[0]?.seasons.find((season) => season.current);
+            if (currentSeason) {
+                setSelectedSeason(currentSeason.year);
+            }
         } catch (error) {
             console.error('Erro ao obter os dados da liga: ', error);
         } finally {
-            setLoading(false);  
+            setLoading(false);
         }
     };
 
@@ -37,63 +45,76 @@ const League = () => {
         fetchLeagueData();
     }, [leagueID]);
 
+    const renderComponent = () => {
+        switch (selected) {
+            case 'classificacoes':
+                return <Standings season={selectedSeason} leagueID={leagueID} />;
+            case 'resultados':
+                return <Results />;
+            case 'lista':
+                return <ListOfGames />;
+            case 'marcadores':
+                return <Statistics season={selectedSeason} leagueID={leagueID} />;
+            default:
+                return <div>Erro</div>;
+        }
+    };
+
+    const handleDropdownSelect = (eventKey) => {
+        setSelectedSeason(eventKey);
+    };
+
     if (loading) {
         return <div>Carregando...</div>;
     }
 
-    const currentSeason = leagueData[0].seasons.find(season => season.current);
-
-    const renderComponent = () => {
-        switch (selected) {
-            case 'classificacoes':
-                return <Standings season={currentSeason.year} leagueID={leagueID} />;
-            case 'resultados':
-                return <Results  />;
-            case 'lista':
-                return <ListOfGames />;
-            case 'marcadores':
-                return <Statistics season={currentSeason.year} leagueID={leagueID} />;
-            case 'arquivo':
-                return <Archive />;
-            default:
-                return <div>Erro</div>
-        }
-    }
-
     return (
-        <Container className='container p-5 rounded-4'>
+        <Container className="container p-5 rounded-4">
             <Row className="align-items-center mb-3">
                 <Col xs="auto">
-                    <Image src={leagueData[0].league.logo} width={60} height={auto} alt="Logo da liga" />
+                    <Image src={leagueData[0]?.league?.logo} width={60} alt="Logo da liga" />
                 </Col>
                 <Col>
-                    <h4 className="mb-0">{leagueData[0].league.name}</h4>
-                    <h6>{currentSeason ? `${currentSeason.year} / ${currentSeason.year + 1}` : 'Nenhuma época atual encontrada'}</h6>
+                    <h4 className="mb-2">{leagueData[0]?.league?.name}</h4>
+                    <Dropdown onSelect={handleDropdownSelect} size="sm">
+                        <Dropdown.Toggle variant="secondary" size="sm" id="dropdown-basic">
+                            {selectedSeason ? `${selectedSeason} / ${parseInt(selectedSeason) + 1}` : 'Selecionar temporada'}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {availableSeasons?.map((season) => (    
+                                <Dropdown.Item eventKey={season}>{`${season} / ${season + 1}`}</Dropdown.Item> 
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Col>
             </Row>
             <Row>
-                <ButtonGroup size='md'>
-                    <Button className={isActiveButton('classificacoes')} onClick={() => handleButtonState('classificacoes')}>
+                <ButtonGroup size="md">
+                    <Button
+                        className={isActiveButton('classificacoes')}
+                        onClick={() => handleButtonState('classificacoes')}
+                    >
                         Classificações
                     </Button>
-                    <Button className={isActiveButton('resultados')} onClick={() => handleButtonState('resultados')}>
+                    <Button
+                        className={isActiveButton('resultados')}
+                        onClick={() => handleButtonState('resultados')}
+                    >
                         Resultados
                     </Button>
                     <Button className={isActiveButton('lista')} onClick={() => handleButtonState('lista')}>
                         Lista
                     </Button>
-                    <Button className={isActiveButton('marcadores')} onClick={() => handleButtonState('marcadores')}>
+                    <Button
+                        className={isActiveButton('marcadores')}
+                        onClick={() => handleButtonState('marcadores')}
+                    >
                         Marcadores
                     </Button>
-                    <Button className={isActiveButton('arquivo')} onClick={() => handleButtonState('arquivo')}>
-                        Arquivo
-                    </Button>
-                </ButtonGroup> 
+                </ButtonGroup>
                 <hr />
             </Row>
-            <Row>
-                {renderComponent()}
-            </Row>
+            <Row>{renderComponent()}</Row>
         </Container>
     );
 };
