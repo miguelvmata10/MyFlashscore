cf407dbef3f6acdc9d2025e77d8f8bbf2ffb51d1
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-12-13 21:51:25 +0000

    Error handling complete in search modal

---
+++ client/src/components/Search/SearchModal.jsx
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+import { Modal, Button as BootstrapButton } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import ToastNotification from './ToastNotification';
+
+const SearchModal = ({ show, onClose }) => {
+
+    const navigate = useNavigate();
+    const [ name, setName ] = useState('Jogador');
+    const [ inputValue, setInputValue ] = useState('');
+    const [ error, setError ] = useState('');
+    const [ showToast, setShowToast ] = useState(false);
+
+    const handleSelect = (selectedName) => {
+        setName(selectedName);
+    };
+
+    const errorMessages = {
+        entryInput: 'Por favor, escreva algo antes de pesquisar.',
+        minLengthTreinador: 'Para pesquisae um treinador tem de escrever pelo menos 4 caracteres.',
+        minLengthDefault: 'Tem de escrever pelo menos 3 caracteres.',
+        invalidQuery: 'A pesquisa não pode conter caracteres especiais.'
+    }
+
+    const validadeInput = (name, input) => {
+        // A API exige um tamanho minimo de 4 caracteres na pesquisa de um treinador
+        const minLength = name === 'Treinador' ? 4 : 3;
+
+        if (input.trim() === '') {
+            return 'entryInput';
+        } else if (input.trim().length < minLength) {
+            return name === 'Treinador' ? 'minLengthTreinador' : 'minLengthDefault';;
+        } else if (/[^a-zA-Z0-9 ]/g.test(input)) {
+            return 'invalidQuery';
+        } else {
+            return null;
+        }
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        const errorKey = validadeInput(name, inputValue);
+        if (errorKey) {
+            setError(errorMessages[errorKey]); // Substitui o marcador de mínimo
+            setShowToast(true);
+        } else {
+            setError('');
+            setShowToast(false);
+            onClose();
+            navigate(`/${name}/${encodeURIComponent(inputValue)}`);
+        }
+    }
+
+    return (
+        <>
+            <ToastNotification showToast={showToast} setShowToast={setShowToast} error={error}/>
+            <Modal show={show} onHide={onClose} size="md" centered>
+            <Modal.Header closeButton className="bg-dark text-white">
+                <Modal.Title id="contained-modal-title-vcenter">
+                Procura
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="bg-dark text-white">
+                <InputGroup className="mb-3">
+                    <Form.Control 
+                        aria-label="Text input with dropdown button"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)} />
+                    <DropdownButton
+                    variant="outline-secondary"
+                    title={name}
+                    id="input-group-dropdown-2"
+                    align="end"
+                    >
+                        <Dropdown.Item onClick={() => handleSelect('Jogador')}>
+                            Jogador
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleSelect('Treinador')}>
+                            Treinador
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleSelect('Clube')}>
+                            Clube
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleSelect('Competicao')}>
+                            Competição
+                        </Dropdown.Item>
+                    </DropdownButton>
+                </InputGroup>
+            </Modal.Body>
+            <Modal.Footer className="bg-dark text-white">
+                <BootstrapButton 
+                    className='btn-custom'
+                    onClick={handleSearch}>
+                        Pesquisar
+                </BootstrapButton>
+            </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default SearchModal
