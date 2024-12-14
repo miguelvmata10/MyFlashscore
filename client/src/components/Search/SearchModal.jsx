83ce56cb86e79f00f18b8a907962121996aaf46f
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-12-14 20:30:50 +0000

    Search field cleaned after being close and the use can search using 'Enter'

--- client/src/components/Search/SearchModal.jsx
+++ client/src/components/Search/SearchModal.jsx
@@ -21,7 +21,7 @@ const SearchModal = ({ show, onClose }) => {
 
     const errorMessages = {
         entryInput: 'Por favor, escreva algo antes de pesquisar.',
-        minLengthTreinador: 'Para pesquisae um treinador tem de escrever pelo menos 4 caracteres.',
+        minLengthTreinador: 'Para pesquisar um treinador tem de escrever pelo menos 4 caracteres.',
         minLengthDefault: 'Tem de escrever pelo menos 3 caracteres.',
         invalidQuery: 'A pesquisa não pode conter caracteres especiais.'
     }
@@ -52,25 +52,41 @@ const SearchModal = ({ show, onClose }) => {
             setError('');
             setShowToast(false);
             onClose();
+            setInputValue('');
             navigate(`/${name}/${encodeURIComponent(inputValue)}`);
         }
+    }
+
+    // Para poder pesquisar também usando o 'Enter'
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch(e);
+        }
+    };
+
+    // Necessário para poder limpar o campo de pesquisa, também quando o utilizador fecha o modal
+    const handleClose = () => {
+        onClose();
+        setInputValue('');
     }
 
     return (
         <>
             <ToastNotification showToast={showToast} setShowToast={setShowToast} error={error}/>
-            <Modal show={show} onHide={onClose} size="md" centered>
-            <Modal.Header closeButton className="bg-dark text-white">
+            <Modal show={show} onHide={handleClose} size="md" centered>
+            <Modal.Header closeButton className="bg-dark text-white" style={{ borderBottom: 'none' }}>
                 <Modal.Title id="contained-modal-title-vcenter">
                 Procura
                 </Modal.Title>
             </Modal.Header>
-            <Modal.Body className="bg-dark text-white">
+            <Modal.Body className="bg-dark text-white modal-body">
                 <InputGroup className="mb-3">
                     <Form.Control 
                         aria-label="Text input with dropdown button"
                         value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)} />
+                        onChange={(e) => setInputValue(e.target.value)} 
+                        onKeyDown={handleKeyPress}
+                    />
                     <DropdownButton
                     variant="outline-secondary"
                     title={name}
@@ -92,7 +108,7 @@ const SearchModal = ({ show, onClose }) => {
                     </DropdownButton>
                 </InputGroup>
             </Modal.Body>
-            <Modal.Footer className="bg-dark text-white">
+            <Modal.Footer className="bg-dark text-white modal-footer">
                 <BootstrapButton 
                     className='btn-custom'
                     onClick={handleSearch}>
