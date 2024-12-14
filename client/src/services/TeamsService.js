6046431985edfec737b2551678db5c4a1e946fd4
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-12-14 20:29:52 +0000

    Teams controller, router and services updated to handle data from team players and coach

--- client/src/services/TeamsService.js
+++ client/src/services/TeamsService.js
@@ -1,9 +1,13 @@
 import { apiRequest } from "./apiServices/apiService";
 
+export const fetchClubData = (teamID) => {
+    return apiRequest(`/teams/info/${teamID}`);
+};
+
 export const fetchSquadInfo = (teamID) => {
     return apiRequest('/teams/squad', {params: {team: teamID}});
 };
 
-export const fetchClubData = (teamID) => {
-    return apiRequest(`/teams/info/${teamID}`);
+export const fetchTeamCoach = (teamID) => {
+    return apiRequest('/teams/coach', {params: {team: teamID}});
 };
--- server/routes/TeamsRoutes.js
+++ server/routes/TeamsRoutes.js
@@ -7,7 +7,10 @@ const teamsController = require("../controllers/TeamsController");
 // rota para obter os dados de um clube através do seu id
 router.get('/info/:id', teamsController.getTeamsInfo);
 
-// rota para obter os dados de um clube através do seu id
+// rota para obter os jogadores de um clube através do seu id
 router.get('/squad', teamsController.getSquadPlayers);
+
+// rota para obter o treinador de um clube através do seu id
+router.get('/coach', teamsController.getSquadCoach);
 
 module.exports = router;
