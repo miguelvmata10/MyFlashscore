5cfdb7c5d0febf38052c889bf1c3e720e314df3f
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-12-18 22:31:25 +0000

    File name changed (previous name was SquadStatistics) and all logic to show all leagues that a teams is in

---
+++ client/src/components/Club/TeamLeaguesSelector.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Row, Container } from 'react-bootstrap';
+import { fetchTeamLeagues } from '../../services/TeamsService';
+import useApiRequest from '../../hooks/useApiRequest';
+import SquadStatistics from './SquadStatistics';
+
+const TeamLeaguesSelector = () => {
+    const { teamID } = useParams()
+    const { data: leagues, loading, error, fetchData } = useApiRequest(fetchTeamLeagues);
+    const [ selectedLeague, setSelectedLeague ] = useState(null);
+    const [ selectedLeagueID, setSelectedLeagueID ] = useState(null);
+    const [ selectedLeagueSeason, setSelectedLeagueSeason ] = useState(null);
+
+    useEffect(() => {
+        if (teamID) {
+            fetchData(teamID);    
+        }
+    }, [teamID, fetchData])
+
+    // Função para filtrar as ligas em que a equipa está inserida na época atual
+    const retrieveCurrentTeamLeagues = (leagues) => {
+        const filteredLeagues = [];
+
+        if (leagues && Array.isArray(leagues)) { 
+            leagues.forEach(({ league, seasons }) => {
+                if (seasons && Array.isArray(seasons)) {
+                    seasons.forEach(({ current, year }) => {
+                        if (current) {
+                            filteredLeagues.push({
+                                id: league.id,
+                                name: league.name,
+                                logo: league.logo,
+                                season: year,
+                            });
+                        }
+                    });
+                }
+            });
+        }
+        return filteredLeagues;
+    };
+    
+    const currentTeamLeagues = retrieveCurrentTeamLeagues(leagues);
+
+    // Inicializa o selectedLeague com o nome da primeira liga, se disponível
+    useEffect(() => {
+        if (currentTeamLeagues.length > 0 && selectedLeague === null) {
+            setSelectedLeague(currentTeamLeagues[0].name);
+            setSelectedLeagueID(currentTeamLeagues[0].id);
+            setSelectedLeagueSeason(currentTeamLeagues[0].season);
+        }
+    }, [currentTeamLeagues, selectedLeague]);
+
+    if (loading) return <p>Carregando...</p>;
+    if (error) return <p>Erro: {error.message}</p>;
+    if (!leagues) return <p>Nenhum dado disponível.</p>;
+
+    const handleDropdownSelect = (eventKey) => {
+        setSelectedLeague(eventKey);
+
+        // para poder atualizar os dados a serem passados para apresentar as estatisticas no componente 'SquadStatistics'
+        const selectedLeague = currentTeamLeagues.find(league => league.name === eventKey);
+        if (selectedLeague) {
+            setSelectedLeagueID(selectedLeague.id);
+            setSelectedLeagueSeason(selectedLeague.season);
+        }
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Dropdown onSelect={handleDropdownSelect}>
+                    <Dropdown.Toggle variant="danger" id="dropdown-basic">
+                        {selectedLeague || "Selecione uma liga"}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="bg-dark text-white">
+                        {currentTeamLeagues.map((league) => (
+                            <Dropdown.Item key={league.id} eventKey={league.name} className="bg-dark text-white">
+                                {league.name}
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Row>
+            <Row>
+                <SquadStatistics leagueID={selectedLeagueID} season={selectedLeagueSeason}/>
+            </Row>
+        </Container>
+  )
+}
+
+export default TeamLeaguesSelector
