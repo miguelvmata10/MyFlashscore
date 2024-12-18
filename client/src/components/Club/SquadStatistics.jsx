1d4a4ef2d3553e8f4593f52a00b1ea5a3564f973
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2024-12-18 22:32:09 +0000

    Component to show the statistics of a team in a certain league

--- client/src/components/Club/SquadStatistics.jsx
+++ client/src/components/Club/SquadStatistics.jsx
@@ -1,42 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { fetchTeamLeagues } from '../../services/TeamsService';
-import useApiRequest from '../../hooks/useApiRequest';
+import { Row, Container } from 'react-bootstrap';
 
-const SquadStatistics = () => {
+const SquadStatistics = ({leagueID, season}) => {
     const { teamID } = useParams()
-    const { data: leagues, loading, error, fetchData } = useApiRequest(fetchTeamLeagues);
-    const [ selectedLeague, setSelectedLeague ] = useState(null);
-
-    useEffect(() => {
-        if (teamID) {
-            fetchData(teamID);    
-        }
-    }, [teamID, fetchData])
-
-    if (loading) return <p>Carregando...</p>;
-    if (error) return <p>Erro: {error.message}</p>;
-    if (!leagues) return <p>Nenhum dado disponível.</p>;
-
-    console.log('DADOS DAS LIGAS DA EQUIPA: ', leagues);
-    console.log('tamanho da resposta da API: ', leagues.length);
-
-    const handleDropdownSelect = (eventKey) => {
-        setSelectedLeague(eventKey);
-    }
-    
     return (
-        <Dropdown onSelect={handleDropdownSelect}>
-            <Dropdown.Toggle variant="danger" id="dropdown-basic">
-                {selectedLeague}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                <Dropdown.Item eventKey='Taça da liga'>Taça da liga</Dropdown.Item>
-                <Dropdown.Item eventKey='Taça'>Taça</Dropdown.Item>
-                <Dropdown.Item eventKey='Champions'>Champions</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-  )
+        <div>Dados para apresentar: liga com o id {leagueID} na época {season} para a equipa com o id { teamID }. </div>
+    )
 }
+
 export default SquadStatistics
