a46ce76a773114f1bfdf25434fb09d045d60247e
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-08 21:11:52 +0000

    Ids of top leagues moved to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,28 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container, Row, Col } from "react-bootstrap";
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
-import Matchlist from './components/Matchlist/Matchlist';
+import MatchDateSelector from './components/Matchlist/MatchDateSelector';
 import League from './components/League/League';
 import Club from './components/Club/Club';
 import PlayerProfile from './components/Members/PlayerProfile';
 import CoachProfile from './components/Members/CoachProfile';
 import SearchResults from './components/Search/SearchResults';
-import './App.css'
 
 function App() {
+  // ids das top leagues
+  const topLeaguesIDs = [39, 140, 135, 78, 2, 94, 61, 3];
+
   return (
     <Router>
       <Container>
         <Navbar />
         <Row>
           <Col xs={12} md={3} >
-            <Sidebar />
+            <Sidebar topLeaguesIDs={topLeaguesIDs}/>
           </Col>
           <Col xs={12} md={9} className="menu p-3 rounded-4 mb-2">
             <Routes>
-              <Route path='/' element={<Matchlist />} />
+              <Route path='/' element={<MatchDateSelector topLeaguesIDs={topLeaguesIDs}/>} />
               <Route path='/league/:leagueID' element={<League />} />
               <Route path='/team/:teamID' element={<Club />} />
               <Route path='/player/:playerID' element={<PlayerProfile />} />
