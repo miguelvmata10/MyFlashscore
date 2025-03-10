6923e1182526a53df537f2a556ba5d40ab523a6d
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-10 22:32:31 +0000

    Matchlist almost complete

--- client/src/components/Matchlist/MatchList.jsx
+++ client/src/components/Matchlist/MatchList.jsx
@@ -1,6 +1,7 @@
 import { ListGroup, Accordion , Row, Col, Image, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import GameCard from '../Game/GameCard';
+import './Matchlist.css';
 
 const MatchList = ({leagueGames, type}) => {
     // apenas as top leagues estarão abertas por default
@@ -12,18 +13,20 @@ const MatchList = ({leagueGames, type}) => {
     console.log('keys:', defaultActiveKeys)
     console.log('LIGA DATA: ', leagueGames);
     return (
-        <Accordion defaultActiveKey={defaultActiveKeys} alwaysOpen={accordionAlwaysOpen} className='m-3'>
+        <Accordion defaultActiveKey={defaultActiveKeys} alwaysOpen={accordionAlwaysOpen}>
             {leagueGames.map((league, index) => (
-                <Accordion.Item eventKey={String(index)} key={index}>
+                <Accordion.Item eventKey={String(index)} key={index} className='bg-transparent mb-2'>
                     <Accordion.Header>
-                        <Image src={league.flag} className='imageResize me-1'/>
+                        <Image src={league.flag} className="imageResize me-1" loading="lazy"/>
                         <Link to={`/league/${league.id}`} className="customLink ms-1">
                             {league.name}
                         </Link>
                     </Accordion.Header>
                     <Accordion.Body className='p-0'>
                         {league.games.map((game, gameIndex) => (
-                        <GameCard GameData={game} key={gameIndex} />
+                            <Link to={`/game/${game.fixture.id}`} key={gameIndex} className='customCardLink'>
+                                <GameCard GameData={game} loading="lazy"/>
+                            </Link>
                         ))}
                     </Accordion.Body>
                 </Accordion.Item>
