f313fa9307933a1a993ba9e60febfc4beacb288c
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-12 00:31:05 +0000

    Team results put in team menu

--- client/src/components/Club/Results.jsx
+++ client/src/components/Club/Results.jsx
@@ -1,6 +1,31 @@
-const Results = () => {
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Row, Container, Image } from 'react-bootstrap';
+import useApiRequest from '../../hooks/useApiRequest';
+import LoadingScreen from '../CommonUI/LoadingScreen';
+import { fetchTeamResults } from "../../services/TeamsService"
+import GameCard from '../Game/GameCard';
+
+const Results = ({leagueID, season}) => {
+  const { teamID } = useParams()
+  const { data: results, loading, error, fetchData } = useApiRequest(fetchTeamResults);
+
+  useEffect(() => {
+    if (teamID && leagueID && season) {
+        fetchData(teamID, leagueID, season);    
+    }
+  }, [teamID, leagueID, season, fetchData])
+
+  if (loading) return <LoadingScreen />;
+  if (error) return <p>Erro: {error.message}</p>;
+  if (!results) return <p>Nenhum dado disponível.</p>;
+
   return (
-    <div>Results</div>
+    <Container className='mt-4'>
+      {results.map((game, gameIndex) => (
+        <GameCard GameData={game} key={gameIndex}/>
+      ))}
+    </Container>
   )
 }
 export default Results
