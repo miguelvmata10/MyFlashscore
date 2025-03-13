1acb34432237a543a855fc6f8f43d499c0c2c551
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-13 00:45:56 +0000

    Game menu partially done
    The result and link to the teams participating in the game is done

--- client/src/components/Game/GameMenu.jsx
+++ client/src/components/Game/GameMenu.jsx
@@ -1,6 +1,118 @@
+import React, { useEffect } from 'react';
+import { Image, Row, Col, ButtonGroup, Button, Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import useButtonGroup from '../../hooks/useButtonGroup';
+import useApiRequest from '../../hooks/useApiRequest';
+import { fetchGame } from '../../services/GameService';
+import LoadingScreen from '../CommonUI/LoadingScreen';
+import GameLineups from './GameLineups';
+import GameSummary from './GameSummary';
+import GameStatistics from './GameStatistics';
+
 const GameMenu = () => {
+  const { fixtureID } = useParams();
+  const { selected, handleButtonState, isActiveButton } = useButtonGroup('sumario');
+  const { data: gameData, loading, error, fetchData } = useApiRequest(fetchGame);
+
+  useEffect(() => {
+      if (fixtureID) {
+          fetchData(fixtureID);
+      }
+  }, [fixtureID, fetchData]);
+
+  if (loading) return <LoadingScreen />;
+  if (error) return <p>Erro: {error.message}</p>;
+  if (!gameData) return <p>Nenhum dado disponível.</p>;
+
+  const game = gameData[0];
+
+  const renderComponent = () => {
+    switch (selected) {
+      case 'sumario':
+          return <GameSummary summary={game.events} />;
+      case 'formacoes':
+          return <GameLineups lineups={game.lineups} />
+      case 'estatisticas':
+          return <GameStatistics stats={game.statistics} />
+      default:
+          return <div>Erro</div>;
+    };
+  }
+
+  console.log('dados do jogo: slb', gameData[0]);
+
   return (
-    <div>GameMenu</div>
+    <Container className="container p-5 rounded-4">
+      <Row className="align-items-start justify-content-center py-3 mb-4">
+        <Col className="text-center">
+          <div className="bg-white rounded-4 d-inline-flex justify-content-center align-items-center p-2" 
+            style={{ width: '100px', height: '100px', maxWidth: '100%', aspectRatio: '1/1' }}>
+              <Image 
+              src={game.teams.home.logo}  
+              fluid 
+              className="mx-auto" 
+              style={{ maxWidth: '80%', maxHeight: '80%' }}
+              />
+          </div>
+          <Link to={`/team/${game.teams.home.id}`} className='customLink'>
+            <h5 className="mt-3">
+                  {game.teams.home.name}
+            </h5>
+          </Link>
+        </Col>
+        
+        <Col className="text-center">
+          <h2 className="display-4 text-danger fw-bold">
+            {game.score.fulltime.home} - {game.score.fulltime.away}
+          </h2>
+        </Col>
+        
+        <Col className="text-center">
+          <div className="bg-white rounded-4 d-inline-flex justify-content-center align-items-center p-2" 
+            style={{ width: '100px', height: '100px', maxWidth: '100%', aspectRatio: '1/1' }}>
+              <Image 
+              src={game.teams.away.logo} 
+              fluid 
+              className="mx-auto" 
+              style={{ maxWidth: '80%', maxHeight: '80%' }}
+              />
+          </div>
+          <Link to={`/team/${game.teams.away.id}`} className='customLink'>
+            <h5 className="mt-3">
+                  {game.teams.away.name}
+            </h5>
+          </Link>
+        </Col>
+      </Row>
+
+      <Row>
+        <ButtonGroup size="md">
+          <Button
+              className={isActiveButton('sumario')}
+              onClick={() => handleButtonState('sumario')}
+          >
+              Sumário
+          </Button>
+          <Button
+              className={isActiveButton('formacoes')}
+              onClick={() => handleButtonState('formacoes')}
+          >
+              Formações
+          </Button>
+          <Button 
+              className={isActiveButton('estatisticas')} 
+              onClick={() => handleButtonState('estatisticas')}
+          >
+              Estatísticas
+          </Button>
+        </ButtonGroup>
+        <hr />
+      </Row>
+      <Row>
+        {renderComponent()}
+      </Row>
+    </Container>
   )
 }
 export default GameMenu
