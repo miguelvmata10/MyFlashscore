00d054287348d43eb1f94c90a4a3f867d31180f5
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-15 18:38:48 +0000

    NotFound component created to be shown when user enter a page with no data available

---
+++ client/src/components/CommonUI/NotFound.jsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+  return (
+    <div className="text-center p-4 fw-bold rounded-4" style={{ backgroundColor: '#4e4e4b', color: 'white' }}>
+        Nenhum evento disponível
+    </div>
+  )
+}
+export default NotFound
