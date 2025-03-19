2bfef332cf2a7ba686f8b13709000698bb1c227c
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-19 00:43:05 +0000

    Less important leagues added to main menu match list

--- client/src/components/Matchlist/LeagueMatchSelector.jsx
+++ client/src/components/Matchlist/LeagueMatchSelector.jsx
@@ -5,10 +5,10 @@ import MatchList from './MatchList';
 import useApiRequest from '../../hooks/useApiRequest';
 import { fetchGamesPerDay } from '../../services/GameService';
 import './MatchList.css';
-import NotFound from '../CommonUI/NotFound';
 
 const LeagueMatchSelector = ({date, topLeaguesIDs}) => {
     const [ topLeagueGames, setTopLeagueGames] = useState([]);
+    const [ otherLeagueGames, setOtherLeagueGames] = useState([]);
     const { data: games, loading, error, fetchData } = useApiRequest(fetchGamesPerDay);
 
     useEffect(() => {
@@ -20,41 +20,58 @@ const LeagueMatchSelector = ({date, topLeaguesIDs}) => {
     const gamesByLeague = useMemo(() => {
         if (!games) return {};
 
-        // Filtra apenas os jogos das ligas prioritárias
-        const priorityGames = games.filter((game) => topLeaguesIDs.includes(game.league?.id));
+        const prioritizeGames = games.reduce((acc, game) => {
+            const league = game.league;
+            const leagueId = league ? league.id : 'outros';
+
+            const isPriority = topLeaguesIDs.includes(leagueId);
+
+            if (!acc[isPriority ? 'priority' : 'other']) {
+                acc[isPriority ? 'priority' : 'other'] = [];
+            }
+
+            acc[isPriority ? 'priority' : 'other'].push(game);
+
+            return acc;
+        }, {priority: [], other: []});
 
         const organizeGameLeaguesByID = (gamesArray) => {
             return gamesArray.reduce((acc, game) => {
+                // Verifica se a liga existe e usa o ID como chave, se não, usa "Outras ligas"
                 const league = game.league;
-                const leagueId = league.id;
-    
+                const leagueId = league ? league.id : 'outros';
+        
                 // Verifica se a chave já existe no acumulador 
                 if (!acc[leagueId]) {
                     acc[leagueId] = {
                         id: leagueId,
                         flag: league.flag,
-                        name: league.name,  
+                        name: league ? league.name : "Outras ligas",  
                         games: []  
                     };
                 }
-    
+        
                 // Adiciona o jogo ao array da liga correspondente
                 acc[leagueId].games.push(game);
                 return acc;
             }, {});
-        };
+        }
 
-        return organizeGameLeaguesByID(priorityGames);
+        const prioritizedLeagueGames = organizeGameLeaguesByID(prioritizeGames.priority);
+        const otherLeagueGames = organizeGameLeaguesByID(prioritizeGames.other);
+
+        return {prioritizedLeagueGames, otherLeagueGames};
 
     }, [games, topLeaguesIDs]);
 
     useEffect(() => {
-        setTopLeagueGames(Object.values(gamesByLeague));
+        setTopLeagueGames(Object.values(gamesByLeague.prioritizedLeagueGames || {}));
+        setOtherLeagueGames(Object.values(gamesByLeague.otherLeagueGames || {}));
     }, [gamesByLeague, games]);
 
     if (loading) return <LoadingScreen />;
     if (error) return <p>Erro: {error.message}</p>;
-    if (!games || games.length === 0 ) return <NotFound />;
+    if (!games) return <p>Nenhum dado disponível.</p>;
     
     return (
         <Container className=''>
@@ -67,12 +84,14 @@ const LeagueMatchSelector = ({date, topLeaguesIDs}) => {
                 </>
             )}
 
-            {/* {otherLeagueGames.length > 0 && (
+            {otherLeagueGames.length > 0 && (
                 <>
-                    <h5>Outras ligas</h5>
+                    <h5 className='border-start border-danger border-4 ps-2 py-1 bg-transparent bg-opacity-50 rounded-start mt-4'>
+                        Outras ligas
+                    </h5>
                     <MatchList leagueGames={otherLeagueGames} type='otherLeagues'/>
                 </>
-            )} */}
+            )}
         </Container>
     )
 }
