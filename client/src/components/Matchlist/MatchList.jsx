cf569807d91555cd7b8b892f1588528ea7f193f7
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-19 00:46:09 +0000

    Lazy loading added in country image

--- client/src/components/Matchlist/MatchList.jsx
+++ client/src/components/Matchlist/MatchList.jsx
@@ -1,4 +1,4 @@
-import { ListGroup, Accordion , Row, Col, Image, Card } from 'react-bootstrap';
+import { Accordion, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import GameCard from '../Game/GameCard';
 import './Matchlist.css';
@@ -8,15 +8,14 @@ const MatchList = ({leagueGames, type}) => {
     // apenas as top leagues estarão abertas por default
     // as ligas com menos importância estarão 'fechadas' para poupar o nº de imagens da API
     const accordionAlwaysOpen = type === 'topLeagues' ? true : false;
-    const defaultActiveKeys = type === 'topLeagues' ? Array.from({ length: leagueGames.length }, (_, index) => String(index))
-    : [];
+    const defaultActiveKeys = type === 'topLeagues' ? Array.from({ length: leagueGames.length }, (_, index) => String(index)) : [];
 
     return (
         <Accordion defaultActiveKey={defaultActiveKeys} alwaysOpen={accordionAlwaysOpen}>
             {leagueGames.map((league, index) => (
                 <Accordion.Item eventKey={String(index)} key={index} className='bg-transparent mb-2'>
                     <Accordion.Header>
-                        <Image src={league.flag} className="imageResize me-1" onError={(e) => (e.currentTarget.src = default_leagueOrCountry_logo)} />
+                        <Image src={league.flag} loading="lazy" className="imageResize me-1" onError={(e) => (e.currentTarget.src = default_leagueOrCountry_logo)} />
                         <Link to={`/league/${league.id}`} className="customLink ms-1">
                             {league.name}
                         </Link>
