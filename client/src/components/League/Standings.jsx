a258ddf7fa2f37de5f6859f7871c376dd2c4961e
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-22 00:46:03 +0000

    Creation of a component for each type of competition
    League component almost done.

--- client/src/components/League/Standings.jsx
+++ client/src/components/League/Standings.jsx
@@ -15,88 +15,180 @@ const formatBadgeGame = (letter) => {
         case 'L':
             return <Badge bg="danger">{letter}</Badge>
         case 'D':
-            return <Badge bg="secondary">{letter}</Badge>
+            return <Badge bg="warning">{letter}</Badge>
         default:
             return <Badge bg="dark">{letter}</Badge>
     }
 }
 
 const formatTeamForm = (form) => {
+    if (!form || form === 'N/A') {
+        return "-";
+    }
     return form.split('').map(letter => formatBadgeGame(letter));
 }
 
-const Standings = ({ season }) => {
+const StandingsTable = ({ teams, hasStandings }) => {
+    // apenas irá mostrar o titulo do frupo, caso haja mais do que 1 grupo
+    const isBiggerThanOneGroup = teams.length > 1;
+    
+    return (
+        <>
+            {hasStandings && teams.map((group, groupIndex) => (
+                <div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }} key={groupIndex}>
+                    {isBiggerThanOneGroup && <h6 className='border-start border-danger border-4 ps-2 py-1 bg-transparent bg-opacity-50 rounded-start'>
+                        {group.groupName}
+                    </h6>}
+                    <Table striped hover variant="dark" className='text-center'>
+                        <thead>
+                            <tr>
+                                <th>º</th>
+                                <th className='text-start'>Equipa</th>
+                                <th>PJ</th>
+                                <th>V</th>
+                                <th>E</th>
+                                <th>D</th>
+                                <th>G</th>
+                                <th>DG</th>
+                                <th>P</th>
+                                <th>Forma</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {group.teams.map((team, teamIndex) => (
+                                <tr className="p-2" key={teamIndex}>
+                                    <td>{team.rank}</td>
+                                    <td className='text-start'>
+                                        <FallbackImage className="imageResize" type='team' src={team.teamLogo} alt="Team logo" />
+                                        <span className="ms-3">
+                                            <Link to={`/team/${team.teamID}`} className="customLink">
+                                                {team.teamName}
+                                            </Link>
+                                        </span>
+                                    </td>
+                                    <td>{team.gamesPlayed}</td>
+                                    <td>{team.gamesWon}</td>
+                                    <td>{team.gamesDrawn}</td>
+                                    <td>{team.gamesLost}</td>
+                                    <td>{`${team.goalsFor}:${team.goalsAgainst}`}</td>
+                                    <td>{team.goalDifference}</td>
+                                    <td>{team.points}</td>
+                                    <td style={{ minWidth: '120px' }}>{formatTeamForm(team.form)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
+            ))} 
+        </>
+    );
+};
+
+const LeagueStandings = ({ teams, hasStandings }) => {
+    return (
+        <StandingsTable teams={ teams } hasStandings={ hasStandings }/>
+    );
+}
+
+const CupStandings = ({ teams, hasStandings }) => {
+    return (
+        <>
+            <div>
+                Cup
+            </div>
+            <StandingsTable teams={ teams } hasStandings={ hasStandings }/>
+        </>
+    );
+}
+
+const Standings = ({ season, type, hasStandings }) => {
     const { leagueID } = useParams();
     const { data: teams, loading, error, fetchData } = useApiRequest(fetchLeagueStanding);
 
     useEffect(() => {
-        if (leagueID && season) {
+        if (leagueID && season && hasStandings) {
             fetchData(leagueID, season);  
         }
-    }, [leagueID, season, fetchData]);
+    }, [leagueID, season, fetchData, hasStandings]);
 
+    // necessário fazer esta lógica aqui pois pode não haver standings para uma competição, mas ainda ser válido
+    // por exemplo, taças não têm standings, mas têm rondas, por isso é necessário colocar esta lógica antes da verificação 
+    // do valor de 'teams' para não renderizar o componente <NotFound />
+    if (!hasStandings) {
+        return (
+            type === 'League' ? <LeagueStandings teams={[]} hasStandings={hasStandings} /> : 
+            type === 'Cup' ? <CupStandings teams={[]} hasStandings={hasStandings} /> : 
+            <div>
+                Erro inesperado nas classificações da competição.
+            </div>
+        );
+    }
+
+
+    // TODO: LÓGICA PARA A CUPSTANDINGS -> verificar hasStandings e o tamanho de teams -> fazer o mesmo para LEAGUESTANDINGS 
+    // RETIFICAR A SITUAÇÃO DOS BOTÕES CASO SE TRATE DE UMA TAÇA SEM STANDINGS
+
+
+
+    // esta parte só será executada se a liga tiver classificações disponíveis
     if (loading) return <LoadingScreen />;
     if (error) return <p>Erro: {error.message}</p>;
     if (!teams || teams.length === 0 ) return <NotFound />;
     
-    return (
-        <div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
-            <Table striped hover variant="dark" className='text-center'>
-              <thead>
-                  <tr>
-                    <th>º</th>
-                    <th className='text-start'>Equipa</th>
-                    <th>PJ</th>
-                    <th>V</th>
-                    <th>E</th>
-                    <th>D</th>
-                    <th>G</th>
-                    <th>DG</th>
-                    <th>P</th>
-                    <th>Forma</th>
-                  </tr>
-              </thead>
-              <tbody>
-                {teams[0].league.standings[0].map((team) => {
-                  const teamData = team?.all;
-                  const teamName = team?.team?.name || 'N/A';
-                  const teamID = team?.team?.id || 'N/A';
-                  const teamLogo = team?.team?.logo || ''; 
-                  const rank = team?.rank || 'N/A';
-                  const gamesPlayed = teamData?.played ?? 'N/A';
-                  const gamesWon = teamData?.win ?? 'N/A';
-                  const gamesDrawn = teamData?.draw ?? 'N/A';
-                  const gamesLost = teamData?.lose ?? 'N/A';
-                  const goalsFor = teamData?.goals?.for ?? 0;
-                  const goalsAgainst = teamData?.goals?.against ?? 0;
-                  const goalDifference = goalsFor - goalsAgainst;
-                  const points = team?.points ?? 'N/A';
-                  const form = team?.form || 'N/A';
+    // garantir que há standings
+    const standings = teams[0]?.league?.standings || [];
 
-                  return (
-                      <tr className="p-2" key={rank}>
-                          <td>{rank}</td>
-                          <td className='text-start'>
-                              <FallbackImage className="imageResize" type='team' src={teamLogo} alt="Team logo" />
-                              <span className="ms-3">
-                                  <Link to={`/team/${teamID}`} className="customLink">
-                                      {teamName}
-                                  </Link>
-                              </span>
-                          </td>
-                          <td>{gamesPlayed}</td>
-                          <td>{gamesWon}</td>
-                          <td>{gamesDrawn}</td>
-                          <td>{gamesLost}</td>
-                          <td>{`${goalsFor}:${goalsAgainst}`}</td>
-                          <td>{goalDifference}</td>
-                          <td>{points}</td>
-                          <td style={{ minWidth: '120px' }}>{formatTeamForm(form)}</td>
-                      </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
+    const groupedStandings = standings.map(( group ) => {
+        // como a API não fornece o nome de cada grupo, fui buscar o nome de cada grupo dentro 
+        // da primeira equipa de cada grupo
+        const groupName = group[0]?.group || 'N/A';
+
+        const teams = group.map((team) => {
+            const teamData = team?.all;
+            const teamName = team?.team?.name || 'N/A';
+            const groupName = team?.group;
+            const teamID = team?.team?.id || 'N/A';
+            const teamLogo = team?.team?.logo || ''; 
+            const rank = team?.rank || 'N/A';
+            const gamesPlayed = teamData?.played ?? 'N/A';
+            const gamesWon = teamData?.win ?? 'N/A';
+            const gamesDrawn = teamData?.draw ?? 'N/A';
+            const gamesLost = teamData?.lose ?? 'N/A';
+            const goalsFor = teamData?.goals?.for ?? 0;
+            const goalsAgainst = teamData?.goals?.against ?? 0;
+            const goalDifference = goalsFor - goalsAgainst;
+            const points = team?.points ?? 'N/A';
+            const form = team?.form || 'N/A';
+
+            return {
+                teamName,
+                groupName,
+                teamID,
+                teamLogo,
+                rank,
+                gamesPlayed,
+                gamesWon,
+                gamesDrawn,
+                gamesLost,
+                goalsFor,
+                goalsAgainst,
+                goalDifference,
+                points,
+                form
+            }
+        })
+
+        return {
+            groupName,
+            teams
+        }
+    });
+
+    return (
+        type === 'League' ? <LeagueStandings teams={groupedStandings} hasStandings={hasStandings} /> : 
+        type === 'Cup' ? <CupStandings teams={groupedStandings} hasStandings={hasStandings} /> : 
+        <div>
+            Erro inesperado nas classificações da competição.
         </div>
     );
 };
