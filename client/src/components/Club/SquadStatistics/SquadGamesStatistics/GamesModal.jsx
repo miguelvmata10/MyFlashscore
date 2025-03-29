78a7d9f6f8de72364d31bd416d3c2d9381114dfd
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-03-29 00:45:04 +0000

    Games and Goal Modals created using the generic modal

---
+++ client/src/components/Club/SquadStatistics/SquadGamesStatistics/GamesModal.jsx
@@ -0,0 +1,58 @@
+import GenericModal from '../../../CommonUI/GenericModal';
+import { Row, ButtonGroup, Button, Container } from 'react-bootstrap';
+import useButtonGroup from "../../../../hooks/useButtonGroup";
+import PointsPerGameOverview from "./PointsPerGameOverview";
+import LineupsStats from './LineupsStats';
+import CardsStats from './CardsStats';
+
+const GamesModal = ({ show, onClose, statistics }) => {
+  const { selected, handleButtonState, isActiveButton } = useButtonGroup('lineups');
+
+  const renderComponent = () => {
+    switch (selected) {
+        case 'lineups':
+            return <LineupsStats lineups={statistics.lineups}/>;
+        case 'ppg':
+            return <PointsPerGameOverview fixtures={statistics.fixtures} />;
+        case 'cards':
+            return <CardsStats cards={statistics.cards} />;
+        default:
+            return <div>Erro</div>;
+    }
+  };
+
+  return (
+    <GenericModal show={show} handleClose={onClose} title='Games statistics' >
+      <Container className='p-2'>
+        <Row>
+          <div className="overflow-auto">
+              <ButtonGroup className="w-100 custom-button mb-3">
+                  <Button
+                      className={isActiveButton('lineups')}
+                      onClick={() => handleButtonState('lineups')}
+                  >
+                      Lineups
+                  </Button>
+                  <Button 
+                      className={isActiveButton('ppg')}
+                      onClick={() => handleButtonState('ppg')}
+                  >
+                      PPG
+                  </Button>
+                  <Button
+                      className={isActiveButton('cards')}
+                      onClick={() => handleButtonState('cards')}
+                  >
+                      Cards
+                  </Button>
+              </ButtonGroup>
+          </div>
+        </Row>
+        <Row>
+          {renderComponent()}
+        </Row>
+      </Container>
+    </GenericModal>
+  )
+}
+export default GamesModal
---
+++ client/src/components/Club/SquadStatistics/SquadGoalsStatistics/GoalsModal.jsx
@@ -0,0 +1,58 @@
+import GenericModal from '../../../CommonUI/GenericModal';
+import { Row, ButtonGroup, Button, Container } from 'react-bootstrap';
+import useButtonGroup from "../../../../hooks/useButtonGroup";
+import GoalsPerMinStats from './GoalsPerMinStats';
+import CleanSheetFailedToScoreStats from './CleanSheetFailedToScoreStats';
+import PenaltiesStats from './PenaltiesStats';
+
+const GoalsModal = ({ show, onClose, statistics }) => {
+  const { selected, handleButtonState, isActiveButton } = useButtonGroup('goalPMin');
+
+  const renderComponent = () => {
+    switch (selected) {
+        case 'goalPMin':
+            return <GoalsPerMinStats goals={statistics.goals}/>;
+        case 'csAndFts':
+            return <CleanSheetFailedToScoreStats statistics={statistics}/>;
+        case 'penalties':
+            return <PenaltiesStats penalty={statistics.penalty}/>;
+        default:
+            return <div>Erro</div>;
+    }
+  };
+
+  return (
+    <GenericModal show={show} handleClose={onClose} title='Goals statistics'>
+      <Container className='p-2'>
+        <Row>
+          <div className="overflow-auto">
+              <ButtonGroup className="w-100 custom-button mb-3">
+                  <Button
+                      className={isActiveButton('goalPMin')}
+                      onClick={() => handleButtonState('goalPMin')}
+                  >
+                      Goal P/Min
+                  </Button>
+                  <Button
+                      className={isActiveButton('penalties')}
+                      onClick={() => handleButtonState('penalties')}
+                  >
+                      Penalties
+                  </Button>
+                  <Button 
+                      className={isActiveButton('csAndFts')}
+                      onClick={() => handleButtonState('csAndFts')}
+                  >
+                      CS & FTS
+                  </Button>
+              </ButtonGroup>
+          </div>
+        </Row>
+        <Row>
+          {renderComponent()}
+        </Row>
+      </Container>
+    </GenericModal>
+  )
+}
+export default GoalsModal
