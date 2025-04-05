ef04d3821f6ad59dcd4c83c515764240c9935c01
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-04-05 22:39:32 +0100

    Error banner component created

---
+++ client/src/components/CommonUI/ErrorBanner.jsx
@@ -0,0 +1,8 @@
+const ErrorBanner = ( errorMessage ) => {
+  return (
+    <div className="text-center p-4 fw-bold rounded-4" style={{ backgroundColor: '#4e4e4b', color: 'white' }}>
+        {errorMessage}
+    </div>
+  )
+}
+export default ErrorBanner
