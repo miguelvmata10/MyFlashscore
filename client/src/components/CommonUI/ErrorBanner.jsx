f22e2a00453e6dfe813d1623e13961c5dfc0fd60
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-04-07 13:20:12 +0100

    Bug fixed in prop object

--- client/src/components/CommonUI/ErrorBanner.jsx
+++ client/src/components/CommonUI/ErrorBanner.jsx
@@ -1,4 +1,4 @@
-const ErrorBanner = ( errorMessage ) => {
+const ErrorBanner = ({ errorMessage }) => {
   return (
     <div className="text-center p-4 fw-bold rounded-4" style={{ backgroundColor: '#4e4e4b', color: 'white' }}>
         {errorMessage}
